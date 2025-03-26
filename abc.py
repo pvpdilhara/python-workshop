c145ec101d41b8ccd9be2944dc4631a34f0a8cdc
unknown <229042@TECH.WYB.AC.LK>
2025-03-26 20:00:30 +0530

    first commit

--- abc.py
+++
@@ -1,3 +0,0 @@
-print("pvpdilhara")
-print("229042")
-print("wayamba university of sri lanaka")
---
+++ abcc.py
@@ -0,0 +1,7 @@
+num1=int(input("enter your number 1:"))
+num2=int(input("enter your number 2:"))
+sum=num1+num2
+div=sum/2
+print(div)
+
+
